79f9aea3b01c3f84c227f06cd2526a7d2a320e0e
DEKC <dekc@outlook.com>
2017-04-17 20:17:36 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# ReduxSimpleStarter
+Basic Redux scaffolding.
+
+Includes:
+* es6 node7 based
+* webpack-dev-server
+* weback-dashboard
+
+```
+> npm install (or Yarn)
+> npm start
+```
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: {
+          loader: 'babel-loader',
+          options: {
+            "presets": ['react', 'env']
+          }
+        },
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
